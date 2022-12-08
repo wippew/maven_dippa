89d1844d7e89c902ef8b98ad517679f9e22e3c86
wippew <victor.pawals@gmail.com>
2022-12-08 18:31:18 +0200

    inbetween commit, not working though

--- src/main/java/fi/testsolver/mavenproject/App.java
+++ src/main/java/fi/testsolver/mavenproject/App.java
@@ -40,16 +40,23 @@ public class App {
 //			
 //		}
 		
-		Double[][] duration = new Double[3][3];
+		Double[][] duration = new Double[4][4];
 		duration[0][0] = 0.0;
-		duration[0][1] = 10.0;
-		duration[0][2] = 15.0;
-		duration[1][0] = 10.0;
-		duration[1][1] = 0.0;
-		duration[1][2] = 15.0;
-		duration[2][0] = 7.0;
-		duration[2][1] = 10.0;
-		duration[2][2] = 0.0;
+	    duration[0][1] = 10.0;
+	    duration[0][2] = 10.0;
+	    duration[0][3] = 10.0;
+	    duration[1][1] = 0.0;
+	    duration[1][0] = 10.0;
+	    duration[1][2] = 10.0;
+	    duration[1][3] = 10.0;
+	    duration[2][2] = 0.0;
+	    duration[2][0] = 10.0;
+	    duration[2][1] = 10.0;
+	    duration[2][3] = 10.0;
+	    duration[3][3] = 0.0;
+	    duration[3][0] = 10.0;
+	    duration[3][1] = 10.0;
+	    duration[3][2] = 10.0;
 
 		
 		int[] demand = {0, 3600, 3600, 3600};
--- src/main/java/fi/testsolver/mavenproject/SOLVE_LP_ORTOOLS.java
+++ src/main/java/fi/testsolver/mavenproject/SOLVE_LP_ORTOOLS.java
@@ -25,7 +25,7 @@ public class SOLVE_LP_ORTOOLS {
 		Loader.loadNativeLibraries();
 		int numberOfNodes = duration[0].length;
 		int depotCount = 1;
-		int maxTime = 10 * 60 * 60;
+		int maxTime = 6 * 60 * 60;
 		int numberOfVehicles = 1;
 		int[] allNodes = IntStream.range(0, numberOfNodes).toArray();
 		int[] allTasks = IntStream.range(1, numberOfNodes).toArray();
@@ -43,7 +43,7 @@ public class SOLVE_LP_ORTOOLS {
 			for(int i: allNodes) {
 				for (int j: allNodes) {
 					if (i != j) {
-						x[i][j][k] = model.makeBoolVar(String.format("x%d_%d, %d", i, j, k));	
+						x[i][j][k] = model.makeBoolVar(String.format("x%d_%d, %d", i, j, k));		
 					}
 				}
 			}
@@ -96,26 +96,26 @@ public class SOLVE_LP_ORTOOLS {
 		
 		//constraint 4: no trip is conducted by a vehicle not belonging to a depot
 		logger.info("Creating Constraint 4...");
-		for (int depotIndex: allDepots) {
-			int [] cars = new int[1];
-			cars[0] = 0;
-			for (int otherDepot: allDepots) {
-				if (depotIndex != otherDepot) {					
-					for (int k = 0; k < cars.length; k++) {
-						MPConstraint constraint0 = model.makeConstraint(0, 0, "c4");
-						int car = cars[k];
-						for (int j: allNodes) {
-							constraint0.setCoefficient(x[otherDepot][j][car], 1);
-						}
-						MPConstraint constraint1 = model.makeConstraint(0, 0, "c4");
-						for (int i: allNodes) {
-							constraint1.setCoefficient(x[i][otherDepot][car], 1);
-						}
-					}
-				}
-			}
-			
-		}
+//		for (int depotIndex: allDepots) {
+//			int [] cars = new int[1];
+//			cars[0] = 0;
+//			for (int otherDepot: allDepots) {
+//				if (depotIndex != otherDepot) {					
+//					for (int k = 0; k < cars.length; k++) {
+//						MPConstraint constraint0 = model.makeConstraint(0, 0, "c4");
+//						int car = cars[k];
+//						for (int j: allNodes) {
+//							constraint0.setCoefficient(x[otherDepot][j][car], 1);
+//						}
+//						MPConstraint constraint1 = model.makeConstraint(0, 0, "c4");
+//						for (int i: allNodes) {
+//							constraint1.setCoefficient(x[i][otherDepot][car], 1);
+//						}
+//					}
+//				}
+//			}
+//			
+//		}
 		
 		
 		//constraint 5: number of vehicles in and out of a tasks's location stays the same
@@ -133,15 +133,15 @@ public class SOLVE_LP_ORTOOLS {
 		}
 		
 		//constraint 6: the time-capacity of each vehicle should not exceed the maximum capacity
-		logger.info("Creating " +  String.valueOf(numberOfNodes) + "Constraint 6...");
-		for (int k: allVehicles) {
-			MPConstraint constraint = model.makeConstraint(0, maxTime, "c6");
-			for (int i: allNodes) {
-				for (int j: allNodes) {
-					constraint.setCoefficient(x[i][j][k] , (duration[i][j] + demand[j]));
-				}
-			}
-		}
+//		logger.info("Creating " +  String.valueOf(numberOfNodes) + "Constraint 6...");
+//		for (int k: allVehicles) {
+//			MPConstraint constraint = model.makeConstraint(0, maxTime, "c6");
+//			for (int i: allNodes) {
+//				for (int j: allNodes) {
+//					constraint.setCoefficient(x[i][j][k] , (duration[i][j] + demand[j]));
+//				}
+//			}
+//		}
 		
 		//definition of auxilliary variable u
 		MPVariable[] u = new MPVariable[numberOfNodes];
@@ -151,17 +151,17 @@ public class SOLVE_LP_ORTOOLS {
 //		
 		// MTZ subtour elimination
 		// first forcing depot to be visited
-//		MPConstraint constraint = model.makeConstraint(1, 1, "c7");
-//		constraint.setCoefficient(u[0], 1);	
-		logger.info("Creating " +  String.valueOf(numberOfNodes) + "Constraint 6...");
+//		MPConstraint constraintTest = model.makeConstraint(1, 1, "c7");
+//		constraintTest.setCoefficient(u[0], 1);	
+		logger.info("Creating " +  String.valueOf(numberOfNodes) + "Constraint 7...");
 		for (int i: allTasks) {
 			for (int j: allTasks) {
 				for (int k: allVehicles) {
 					if (i != j) {
-						MPConstraint constraint = model.makeConstraint(-10, numberOfNodes, "c6 "+ String.format("%d_%d", i,j));
+						MPConstraint constraint = model.makeConstraint(-numberOfNodes+1, numberOfNodes, "c7 "+ String.format("%d_%d", i,j));
 						constraint.setCoefficient(u[j], 1);
 						constraint.setCoefficient(u[i], -1);
-						constraint.setCoefficient(x[i][j][k], -6);
+						constraint.setCoefficient(x[i][j][k], -numberOfNodes);
 					}
 				}
 			}
@@ -186,6 +186,9 @@ public class SOLVE_LP_ORTOOLS {
 		
 		MPSolver.ResultStatus resultStatus = model.solve();
 		
+		String test2 = model.exportModelAsLpFormat();
+		System.out.println(test2);
+		
 		
 		List<String> k0 = new ArrayList<>();
 		List<String> k1 = new ArrayList<>();
@@ -197,16 +200,21 @@ public class SOLVE_LP_ORTOOLS {
 		  for (int k: allVehicles) {
 			  for(int i: allNodes) {
 				  for (int j: allNodes) {
+					  
 					  if (i != j && x[i][j][k].solutionValue() == 1.0) {
-						  if (k == 0) {
-							  k0.add(String.format("%d_%d", i, j));
-						  } else {
-							  k1.add(String.format("%d_%d", i, j));
-						  }
+						  double testtest = x[i][j][k].solutionValue();
+						  k0.add(String.format("%d_%d", i, j));
+						  System.out.println("ASDASD");
+					  }
+						  
+//						  if (k == 0) {
+//							  k0.add(String.format("%d_%d", i, j));
+//						  } else {
+//							  k1.add(String.format("%d_%d", i, j));
+//						  }
 					  }
 				  }
 			  }
-		  }
 		} else {
 		  System.err.println("No solution found.");
 		}
